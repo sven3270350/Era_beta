d748e9a9ce9e1ed4edca7b53365ded02ac8d4edb
sven3270350 <sven3270350@gmail.com>
2023-10-19 10:56:59 -0400

    fix build error

--- src/components/RakingPage/RakingTab/RakingTab.jsx
+++ src/components/RakingPage/RakingTab/RakingTab.jsx
@@ -10,44 +10,59 @@ const RakingTab = ({ data }) => {
     <Tabs value={data[activeTab].value} onChange={setActiveTab}>
       <TabsHeader>
         {data.map(({ label, value }, index) => (
-          <Tab
-            key={value}
-            value={value}
-            className="py-12"
-            // style={{ borderBottom: activeTab === index ? '3px solid #b780ff8c' : 'none' }}
+          <div className="relative w-full">
+            <Tab
+              key={value}
+              value={value}
+              className="z-20 w-full py-12"
+              onClick={() => setActiveTab(index)}
             >
-            {label}
-          </Tab>
+              {label}
+            </Tab>
+            <img
+              className={
+                activeTab === index ? 'absolute z-10 flex w-full' : 'absolute z-0 hidden w-full'
+              }
+              style={{ top: '4.82px', left: '0px', height: '171%' }}
+              width={56}
+              src={index === 0 ? '/assets/images/first_tab.png' : '/assets/images/last_tab.png'}
+              alt=""
+            />
+          </div>
         ))}
       </TabsHeader>
       <TabsBody
         style={{
-          border: '3px solid #b780ff8c',
-          borderTop: 'none',
-          backgroundColor: '#b780ff14',
-          borderEndEndRadius: '40px',
-          borderBottomLeftRadius: '40px',
+          backgroundImage: 'url("/assets/images/tab_body.png")',
+          // height: "100%",
+          // width: "100%",
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center center',
         }}
       >
-          <TabPanel
-            key={"All collections"}
-            value={"All collections"}
-            className=""
-            style={{ height: 'fit-content' }}
+        <TabPanel
+          key={'All collections'}
+          value={'All collections'}
+          className=""
+          style={{ height: '697px' }}
+        >
+          <RakingTable />
+        </TabPanel>
+        <TabPanel
+          key={'My watchlist'}
+          value={'My watchlist'}
+          className="flex items-center justify-center"
+          style={{ height: '697px' }}
+        >
+          <WalletNotConnected width="72" height="72" />
+          <div
+            className="nato-fontfamily text-2xl font-black"
+            style={{ marginLeft: '32px', lineHeight: '28.8px' }}
           >
-            <RakingTable />
-          </TabPanel>
-          <TabPanel
-            key={"My watchlist"}
-            value={"My watchlist"}
-            className="flex items-center justify-center"
-            style={{ height: 'fit-content' }}
-          >
-            <WalletNotConnected width="72" height="72" />
-            <div className="nato-fontfamily font-black text-2xl" style={{ marginLeft: '32px', lineHeight: '28.8px' }}>
-              My watchlist
-            </div>
-          </TabPanel>
+            My watchlist
+          </div>
+        </TabPanel>
       </TabsBody>
     </Tabs>
   );
